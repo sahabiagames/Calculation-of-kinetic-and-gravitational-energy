77d64b2483ae86058707982f090b5245313ddd67
sahabia <sahabia-games@outlook.com>
2022-02-06 12:10:20 +0330

    upadte game...

--- c2runtime.js
+++ c2runtime.js
@@ -19829,6 +19829,229 @@ cr.plugins_.Touch = function(runtime)
 }());
 ;
 ;
+cr.plugins_.XML = function(runtime)
+{
+	this.runtime = runtime;
+	if (this.runtime.isIE)
+	{
+		var x = {};
+		window["XPathResult"] = x;
+		x.NUMBER_TYPE = 1;
+		x.STRING_TYPE = 2;
+		x.UNORDERED_NODE_SNAPSHOT_TYPE = 6;
+		x.ORDERED_NODE_SNAPSHOT_TYPE = 7;
+	}
+};
+(function ()
+{
+	var pluginProto = cr.plugins_.XML.prototype;
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	var typeProto = pluginProto.Type.prototype;
+	typeProto.onCreate = function()
+	{
+	};
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	var instanceProto = pluginProto.Instance.prototype;
+	instanceProto.onCreate = function()
+	{
+		this.xmlDoc = null;
+		this.nodeStack = [];
+		if (this.runtime.isDomFree)
+			cr.logexport("[Construct 2] The XML object is not supported on this platform.");
+	};
+	instanceProto.xpath_eval_one = function(xpath, result_type)
+	{
+		if (!this.xmlDoc)
+			return;
+		var root = this.nodeStack.length ? this.nodeStack[this.nodeStack.length - 1] : this.xmlDoc.documentElement;
+		try {
+			if (this.runtime.isIE)
+				return root.selectSingleNode(xpath);
+			else
+				return this.xmlDoc.evaluate(xpath, root, null, result_type, null);
+		}
+		catch (e) { return null; }
+	};
+	instanceProto.xpath_eval_many = function(xpath, result_type)
+	{
+		if (!this.xmlDoc)
+			return;
+		var root = this.nodeStack.length ? this.nodeStack[this.nodeStack.length - 1] : this.xmlDoc.documentElement;
+		try {
+			if (this.runtime.isIE)
+				return root.selectNodes(xpath);
+			else
+				return this.xmlDoc.evaluate(xpath, root, null, result_type, null);
+		}
+		catch (e) { return null; }
+	};
+	function Cnds() {};
+	instanceProto.doForEachIteration = function (current_event, item)
+	{
+		this.nodeStack.push(item);
+		this.runtime.pushCopySol(current_event.solModifiers);
+		current_event.retrigger();
+		this.runtime.popSol(current_event.solModifiers);
+		this.nodeStack.pop();
+	};
+	Cnds.prototype.ForEach = function (xpath)
+	{
+		if (this.runtime.isDomFree)
+			return false;
+		var current_event = this.runtime.getCurrentEventStack().current_event;
+		var result = this.xpath_eval_many(xpath, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE);
+		var i, len, x;
+		if (!result)
+			return false;
+		else
+		{
+			var current_loop = this.runtime.pushLoopStack();
+			if (this.runtime.isIE)
+			{
+				for (i = 0, len = result.length; i < len; i++)
+				{
+					current_loop.index = i;
+					this.doForEachIteration(current_event, result[i]);
+				}
+			}
+			else
+			{
+				for (i = 0, len = result.snapshotLength; i < len; i++)
+				{
+					current_loop.index = i;
+					this.doForEachIteration(current_event, result.snapshotItem(i));
+				}
+			}
+			this.runtime.popLoopStack();
+		}
+		return false;
+	};
+	pluginProto.cnds = new Cnds();
+	function Acts() {};
+	Acts.prototype.Load = function (str)
+	{
+		if (this.runtime.isDomFree)
+			return;
+		var xml, tmp;
+		var isWindows8 = !!(typeof window["c2isWindows8"] !== "undefined" && window["c2isWindows8"]);
+		try {
+			if (isWindows8)
+	        {
+	            xml = new Windows["Data"]["Xml"]["Dom"]["XmlDocument"]()
+	            xml["loadXml"](str);
+	        }
+			else if (this.runtime.isIE)
+			{
+				var versions = ["MSXML2.DOMDocument.6.0",
+                        "MSXML2.DOMDocument.3.0",
+                        "MSXML2.DOMDocument"];
+				for (var i = 0; i < 3; i++){
+					try {
+						xml = new ActiveXObject(versions[i]);
+						if (xml)
+							break;
+					} catch (ex){
+						xml = null;
+					}
+				}
+				if (xml)
+				{
+					xml.async = "false";
+					xml["loadXML"](str);
+				}
+			}
+			else {
+				tmp = new DOMParser();
+				xml = tmp.parseFromString(str, "text/xml");
+			}
+		} catch(e) {
+			xml = null;
+		}
+		if (xml)
+		{
+			this.xmlDoc = xml;
+			if (this.runtime.isIE && !isWindows8)
+				this.xmlDoc["setProperty"]("SelectionLanguage","XPath");
+		}
+	};
+	pluginProto.acts = new Acts();
+	function Exps() {};
+	Exps.prototype.NumberValue = function (ret, xpath)
+	{
+		if (this.runtime.isDomFree)
+		{
+			ret.set_int(0);
+			return;
+		}
+		var result = this.xpath_eval_one(xpath, XPathResult.NUMBER_TYPE);
+		if (!result)
+			ret.set_int(0);
+		else if (this.runtime.isIE)
+			ret.set_int(parseInt(result.nodeValue, 10));
+		else
+			ret.set_int(result.numberValue || 0);
+	};
+	Exps.prototype.StringValue = function (ret, xpath)
+	{
+		if (this.runtime.isDomFree)
+		{
+			ret.set_string("");
+			return;
+		}
+		var result;
+		if (/firefox/i.test(navigator.userAgent))
+		{
+			result = this.xpath_eval_one(xpath, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE);
+			if (!result)
+				ret.set_string("");
+			else
+			{
+				var i, len, totalstr = "";
+				for (i = 0, len = result.snapshotLength; i < len; i++)
+				{
+					totalstr += result.snapshotItem(i).textContent;
+				}
+				ret.set_string(totalstr);
+			}
+		}
+		else
+		{
+			result = this.xpath_eval_one(xpath, XPathResult.STRING_TYPE);
+			if (!result)
+				ret.set_string("");
+			else if (this.runtime.isIE)
+				ret.set_string((result.nodeValue || result.nodeTypedValue) || "");
+			else
+				ret.set_string(result.stringValue || "");
+		}
+	};
+	Exps.prototype.NodeCount = function (ret, xpath)
+	{
+		if (this.runtime.isDomFree)
+		{
+			ret.set_int(0);
+			return;
+		}
+		var result = this.xpath_eval_many(xpath, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE);
+		if (!result)
+			ret.set_int(0);
+		else if (this.runtime.isIE)
+			ret.set_int(result.length || 0);
+		else
+			ret.set_int(result.snapshotLength || 0);
+	};
+	pluginProto.exps = new Exps();
+}());
+;
+;
 cr.plugins_.gamepad = function(runtime)
 {
 	this.runtime = runtime;
@@ -20412,6 +20635,7 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.TextBox,
 	cr.plugins_.TiledBg,
 	cr.plugins_.Touch,
+	cr.plugins_.XML,
 	cr.system_object.prototype.cnds.OnLayoutStart,
 	cr.plugins_.TextBox.prototype.acts.SetCSSStyle,
 	cr.plugins_.TextBox.prototype.acts.SetText,
--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1644136751,
+	"version": 1644136803,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
